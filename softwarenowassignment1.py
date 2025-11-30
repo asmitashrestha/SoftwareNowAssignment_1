885723e5fc3d8bd82dd9d9773798fb61c0192108
unknown <user.name>
2025-11-30 17:05:10 +0930

    feat:make a condtion for strong password and add a variables to check the uppercase characters

--- softwarenowassignment1.py
+++ softwarenowassignment1.py
@@ -9,16 +9,20 @@ inputPassword = input("Enter a password:") #ask the user to input a password
 def check_password_strength(password):
   length=len(password) #variable define to check the length of the password
   check_digit= any(ch.isdigit() for ch in password) #check_digit is a variable that is defined to check a  digit in a password,
-                                                    #isdigit() is a python built in function that is used to check wheather it is digit or not
+  check_uppercaseletter =any(ch.isupper() for ch in password)                                                #isdigit() is a python built in function that is used to check wheather it is digit or not
                                                     #for loop is used to iterate the password and check wheather there is a digit present in a password or not
-  if length < 6:
+  if length < 6:  #if length of the password is less than 6 then this condtion will run and return the message password is weak
     return "Password is weak"
-  elif 6<=length<=10:
-    if check_digit:
+  elif 6<=length<=10:   # if length of the password is more than or equal to 6 but not more than then this will run
+    if check_digit: #if above condition verifies and password contain at least one digit then this condition is true
        return "Password is medium"
+    else:  #if password contain 6 or more than 6 characters but doesn't contain digit then this condition will be true
+      return "Password is weak"
+  else:  # if all conditions of above became false then this condtion is true and return password is weak
+    if length>10 and check_digit and check_uppercaseletter:
+      return "Password is strong"
     else:
       return "Password is weak"
-  else:
-    return "Password is weak"
-strength_of_password = check_password_strength(inputPassword)
-print("password strength:", strength_of_password)
+    
+strength_of_password = check_password_strength(inputPassword) #call the function check_password_strength(inputpassword) to place the password
+print("password strength:", strength_of_password) #this line will print the password strength i.e; weak,medium and strong
